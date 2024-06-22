d51c3acc9239f26d0427f82ac2a80d73f751f9ef
zbx <2694735157@qq.com>
2024-06-22 14:09:49 +0800

    部分功能实现

--- src/components/action-button.vue
+++ src/components/action-button.vue
@@ -2,14 +2,17 @@
 defineProps<{
   icon: string
   label: string
+  active?: boolean
 }>()
 </script>
 
 <template>
   <div class="flex justify-center py-2">
     <div class="flex items-center justify-center flex-col hover:cursor-pointer">
-      <m-icon :name="icon" :size="28"></m-icon>
-      <span class="text-size-[14px] text-gray-500 mt-1"> {{ label }} </span>
+      <m-icon :name="icon" :size="28" :color="active ? 'red' : undefined"></m-icon>
+      <span class="text-size-[14px] text-gray-500 mt-1 select-none" :class="active ? 'text-red' : ''">
+        {{ label }}
+      </span>
     </div>
   </div>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import { Application } from '@manycore/custom-ksg-viewer-sdk'
-import {
-  Menu as MenuIcon,
-  ScanAlt,
-  ModelAlt,
-  CicsExplorer,
-  Document,
-  LogoGlassdoor,
-  QrCode,
-  Blog
-} from '@vicons/carbon'
+import { Application, ERenderMode } from '@manycore/custom-ksg-viewer-sdk'
+import { router } from '@/router'
+import { debounce } from 'lodash'
 
 const sceneRef = ref<null | HTMLDivElement>()
 let app: Application | null = null
 
 const showLoading = ref(true)
 const actionShow = ref(false)
+const state = ref({
+  floorplanShow: false,
+  doorShow: true,
+  showModal: false,
+  bomValue: 0
+})
 
 async function loadScene(designId: string, orderId: number) {
   showLoading.value = true
@@ -25,18 +23,51 @@ async function loadScene(designId: string, orderId: number) {
 
   await app!.sceneService.viewAsync({
     designId: designId,
-    modelId: topModels.map((v) => v.topModelId)
+    modelId: topModels.map((v) => v.topModelId),
+    withFloorplan: true
   })
+
+  app!.sceneService.config.renderMode.set(ERenderMode.OUTLINE_WITH_SHADING)
+  app!.floorplanService.switchVisibility(state.value.floorplanShow)
+  app!.sceneService.resetPerspective()
+
   showLoading.value = false
+}
+
+function displayFloorplan() {
+  state.value.floorplanShow = !state.value.floorplanShow
+  app!.floorplanService.switchVisibility(state.value.floorplanShow)
+  actionShow.value = false
+}
+
+function displayDoor() {
+  state.value.doorShow = !state.value.doorShow
+  app!.visibilityService.setVisibility((it) => it.prodCatId === 498 || it.prodCatId === 708, state.value.doorShow)
+  actionShow.value = false
+}
+
+function showModelBom() {
+  actionShow.value = false
+  state.value.showModal = true
 }
 
 onMounted(() => {
   // 创建场景
   app = new Application(sceneRef.value!)
+  const reload = debounce(() => {
+    app!.sceneService.resize()
+  }, 100)
   // 窗口变化时重绘场景
   window.addEventListener('resize', () => {
-    app!.sceneService.resize()
+    reload()
   })
+
+  watch(
+    () => state.value.bomValue,
+    (val) => {
+      app?.modelService.explode({ scale: val })
+    }
+  )
 
   loadScene('3FO3QRQEX9DD', 20240508373567)
 })
@@ -59,23 +90,43 @@ onMounted(() => {
         <m-icon name="carbon:menu" />
       </n-float-button>
       <n-drawer v-model:show="actionShow" placement="bottom">
-        <n-drawer-content title="功能菜单" style="--n-body-padding: 0">
-          <n-scrollbar>
-            <div class="grid justify-center gap-2 px-3 py-4" style="grid-template-columns: repeat(auto-fill, 80px)">
-              <action-button icon="carbon:scan-alt" label="扫描条码" />
-              <action-button icon="carbon:model-alt" label="显示模式" />
+        <n-drawer-content title="功能菜单" style="--n-body-padding: 0" :native-scrollbar="false">
+          <div class="grid justify-center gap-2 px-3 py-4" style="grid-template-columns: repeat(auto-fill, 80px)">
+            <action-button icon="carbon:scan-alt" label="扫描条码" />
+            <action-button icon="carbon:model-alt" label="显示模式" />
 
-              <action-button icon="carbon:blog" label="显示户型" />
-              <action-button icon="carbon:qr-code" label="输入条码" />
-              <action-button icon="carbon:document" label="板件清单" />
-              <action-button icon="carbon:cics-explorer" label="模型爆炸" />
-              <action-button icon="carbon:logo-glassdoor" label="显示门板" />
-            </div>
-          </n-scrollbar>
+            <action-button
+              icon="carbon:blog"
+              label="显示户型"
+              :active="state.floorplanShow"
+              @click="displayFloorplan"
+            />
+            <action-button icon="carbon:qr-code" label="输入条码" />
+            <action-button icon="carbon:document" label="板件清单" />
+            <action-button icon="carbon:cics-explorer" label="模型爆炸" @click="showModelBom" />
+            <action-button
+              icon="carbon:logo-glassdoor"
+              :active="state.doorShow"
+              label="显示门板"
+              @click="displayDoor"
+            />
+          </div>
         </n-drawer-content>
       </n-drawer>
       <!-- 场景canvas -->
-      <div ref="sceneRef" class="flex-1 bg-amber"></div>
+      <div ref="sceneRef" class="flex-1 bg-[#f0f0f0] overflow-hidden"></div>
+
+      <n-modal v-model:show="state.showModal">
+        <n-card
+          style="width: 100%; position: fixed; bottom: 0"
+          title="爆炸范围"
+          :bordered="false"
+          role="dialog"
+          aria-modal="true"
+        >
+          <n-slider v-model:value="state.bomValue" :step="1" />
+        </n-card>
+      </n-modal>
     </div>
   </n-spin>
 </template>
